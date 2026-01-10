a717c291f8e73b77c29fd11ca05fbcdb16a5c6c8
Brzenczyk <104158766+Brzenczyk@users.noreply.github.com>
2026-01-10 15:05:45 +0100

    fix prevent mailer from crashing app

--- mailer.js
+++ mailer.js
@@ -3,19 +3,25 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
     port: Number(process.env.MAIL_PORT),
+    secure: false,
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS
     }
 });
 
-async function sendMail(subject, text) {
-    await transporter.sendMail({
-        from: '"D&D Planner 🎲" <no-reply@dnd-planner.local>',
-        to: "test@mailtrap.io", // Mailtrap
-        subject,
-        text
-    });
+async function sendMail(to, subject, text) {
+    try {
+        await transporter.sendMail({
+            from: '"D&D Planner 🎲" <no-reply@dnd-planner.local>',
+            to,
+            subject,
+            text
+        });
+    } catch (err) {
+        console.error("MAIL ERROR:", err.message);
+        // ❗ NIE RZUCAJEMY błędu dalej
+    }
 }
 
 module.exports = { sendMail };
