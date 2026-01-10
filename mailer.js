92b11424f30b2630b7a8fdf35bbf48e4bb20fae5
Brzenczyk <104158766+Brzenczyk@users.noreply.github.com>
2026-01-10 16:59:14 +0100

    Fix email test

--- mailer.js
+++ mailer.js
@@ -1,31 +1,33 @@
 const nodemailer = require("nodemailer");
+require("dotenv").config();
 
-let transporter;
+let transporter = null;
 
-if (!process.env.MAIL_USER) {
-    console.log("Mail nie skonfigurowany, wysyłanie maili wyłączone");
-    transporter = null;
-} else {
+if (process.env.MAIL_USER && process.env.MAIL_PASS && process.env.MAIL_HOST && process.env.MAIL_PORT) {
     transporter = nodemailer.createTransport({
         host: process.env.MAIL_HOST,
         port: Number(process.env.MAIL_PORT),
-        secure: false,
         auth: {
             user: process.env.MAIL_USER,
             pass: process.env.MAIL_PASS
         }
     });
+} else {
+    console.log("Mailtrap nie skonfigurowany, wysyłanie maili wyłączone");
 }
 
 async function sendMail(subject, text) {
     if (!transporter) return;
+
     try {
         await transporter.sendMail({
-            from: '"D&D Planner 🎲" <no-reply@dnd-planner.local>',
-            to: process.env.MAIL_USER,
-            subject,
-            text
-        });
+    from: '"D&D Planner 🎲" <no-reply@dnd-planner.local>',
+    to: process.env.MAIL_TO,   // teraz odbiorca z .env
+    subject,
+    text
+});
+
+        console.log("Mail wysłany:", subject);
     } catch (err) {
         console.error("MAIL ERROR:", err.message);
     }
